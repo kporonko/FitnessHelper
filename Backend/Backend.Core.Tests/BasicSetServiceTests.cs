7ade187a5e465475032d6f7469107330942da82f
KeeYoo <poronko2045@gmail.com>
2022-09-20 10:08:04 +0300

    Tests

---
+++ Backend/Backend.Core.Tests/BasicSetServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using Backend.Core.Models;
+using Backend.Core.Services;
+using Backend.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Backend.Core.Tests
+{
+    public class BasicSetServiceTests
+    {
+        [Fact]
+        public void GetBasicalSetsSmallInfoBySection_ExistingSection_ReturnListOfExercises()
+        {
+            // Arrange
+            ApplicationContextFactory applicationContextFactory = new ApplicationContextFactory();
+            var service = new BasicSetService(applicationContextFactory.CreateDbContext(new string[] { }));
+
+            // Act
+            var list = service.GetBasicalSetsSmallInfoBySection(1);
+
+            // Assert
+            Assert.NotNull(list);
+        }
+
+        [Fact]
+        public void GetBasicalSetsSmallInfoBySection_NonExistingSection_ReturnNull()
+        {
+            // Arrange
+            ApplicationContextFactory applicationContextFactory = new ApplicationContextFactory();
+            var service = new BasicSetService(applicationContextFactory.CreateDbContext(new string[] { }));
+
+            // Act
+            var list = service.GetBasicalSetsSmallInfoBySection(10);
+
+            // Assert
+            Assert.Null(list);
+        }
+
+        [Fact]
+        public void GetBasicalSetFullDescById_ExistingId_ReturnBasicalSet()
+        {
+            // Arrange
+            ApplicationContextFactory applicationContextFactory = new ApplicationContextFactory();
+            var service = new BasicSetService(applicationContextFactory.CreateDbContext(new string[] { }));
+
+            // Act
+            var set = service.GetBasicalSetFullDescById(1);
+
+            // Assert
+            Assert.Equal(1, set.Id);
+        }
+
+        [Fact]
+        public void GetBasicalSetFullDescById_NonExistingId_ReturnNull()
+        {
+            // Arrange
+            ApplicationContextFactory applicationContextFactory = new ApplicationContextFactory();
+            var service = new BasicSetService(applicationContextFactory.CreateDbContext(new string[] { }));
+
+            // Act
+            var set = service.GetBasicalSetsSmallInfoBySection(10000);
+
+            // Assert
+            Assert.Null(set);
+        }
+    }
+}
---
+++ Backend/Backend.Core.Tests/UserServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using Backend.Core.Models;
+using Backend.Core.Services;
+using Backend.Infrastructure.Data;
+using Xunit;
+
+namespace Backend.Core.Tests
+{
+    public class UserServiceTests
+    {
+        [Fact]
+        public void UserGet_NonExistingUser_ReturnNull()
+        {
+            // Arrange
+            ApplicationContextFactory applicationContextFactory = new ApplicationContextFactory();
+            var service = new UserService(applicationContextFactory.CreateDbContext(new string[] { }));
+            var userLogin = new LoginUser() { Login = "TestLogin" + (char)new Random().Next(1,100), Password = "TestPassword" + (char)new Random().Next(1, 100) };
+
+            // Act
+            var user = service.Get(userLogin);
+
+            // Assert
+            Assert.Null(user);
+        }
+
+        [Fact]
+        public void UserGet_ExistingUser_ReturnId()
+        {
+            // Arrange
+            ApplicationContextFactory applicationContextFactory = new ApplicationContextFactory();
+            var service = new UserService(applicationContextFactory.CreateDbContext(new string[] { }));
+            var userLogin = new LoginUser() { Login = "amonullo@gmail.com", Password = "12345678" };
+
+            // Act
+            var user = service.Get(userLogin);
+
+            // Assert
+            Assert.Equal(1, user.UserId);
+        }
+
+        [Fact]
+        public void UserCreate_UserWithExistingLogin_NotCreatedUser()
+        {
+            // Arrange
+            ApplicationContextFactory applicationContextFactory = new ApplicationContextFactory();
+            var service = new UserService(applicationContextFactory.CreateDbContext(new string[] { }));
+            var userRegister = new RegisterUser() { Login = "amonullo@gmail.com", Password = "TestPassword", FirstName = "TestFirstName", LastName = " TestLastName" };
+
+            // Act
+            var statusCode = service.Create(userRegister);
+
+            // Assert
+            Assert.Equal(409, (int)statusCode);
+        }
+
+        [Fact]
+        public void UserCreate_UserWithNonExistingLogin_CreatedUser()
+        {
+            // Arrange
+            ApplicationContextFactory applicationContextFactory = new ApplicationContextFactory();
+            var service = new UserService(applicationContextFactory.CreateDbContext(new string[] { }));
+            var userRegister = new RegisterUser() { Login = "TestLogin", Password = "TestPassword", FirstName = "TestFirstName", LastName = "TestLastName" };
+
+            // Act
+            var statusCode = service.Create(userRegister);
+
+            // Assert
+            Assert.Equal(201, (int)statusCode);
+        }
+    }
+}
