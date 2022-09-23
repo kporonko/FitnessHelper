9a2fe3386529ba220652a07f411884611d102f50
KeeYoo <poronko2045@gmail.com>
2022-09-23 17:31:33 +0300

    CSS fix: added responsiveness to forms and login&register pages

--- Frontend/frontend/src/pages/Login.tsx
+++ Frontend/frontend/src/pages/Login.tsx
@@ -26,23 +26,25 @@ const Login = () => {
     return (
         <div className={classes.mainDiv}>
             <StartImage/>
-            <form onSubmit={(e)=> submit(e)} className={classes.formLogin}>
-                <h1>Sign In To Your Account</h1>
-                <div className={classes.inputwrapper}>
-                    <label htmlFor="Login">Login</label>
-                    <input type="text" placeholder="Enter login..." id="Login" value={login} onChange={(e)=>{setLogin(e.target.value)}}/>
-                </div>
-                <div className={classes.inputwrapper}>
-                    <label htmlFor="Password">Password</label>
-                    <input type="password" placeholder="Enter password..." id="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-                </div>
-                <button className={classes.smallBtn}>
-                    <Link className={classes.textDecNone} to={"/register"}>I don`t have an account</Link>
-                </button>
-                <button type="submit" disabled={login === "" || password === "" || password.length < 8 && true} className={login === "" || password === "" || password.length < 8 ? classes.loginBtnDisabled : classes.loginBtnActive}>
-                    <h5>Login</h5>
-                </button>
-            </form>
+            <div style={{width: "-webkit-fill-available", margin: "150px 40px"}}>
+                <form onSubmit={(e)=> submit(e)}>
+                    <h1>Sign In To Your Account</h1>
+                    <div className={classes.inputwrapper}>
+                        <label htmlFor="Login">Login</label>
+                        <input type="text" placeholder="Enter login..." id="Login" value={login} onChange={(e)=>{setLogin(e.target.value)}}/>
+                    </div>
+                    <div className={classes.inputwrapper}>
+                        <label htmlFor="Password">Password</label>
+                        <input type="password" placeholder="Enter password..." id="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                    </div>
+                    <button className={classes.smallBtn}>
+                        <Link className={classes.textDecNone} to={"/register"}>I don`t have an account</Link>
+                    </button>
+                    <button type="submit" disabled={login === "" || password === "" || password.length < 8 && true} className={login === "" || password === "" || password.length < 8 ? classes.loginBtnDisabled : classes.loginBtnActive}>
+                        <h5>Login</h5>
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
--- Frontend/frontend/src/pages/Register.tsx
+++ Frontend/frontend/src/pages/Register.tsx
@@ -26,33 +26,35 @@ const Register = () => {
     return (
         <div className={classes.mainDiv}>
             <StartImage/>
-            <form onSubmit={(e)=> submit(e)} className={classes.formRegister}>
-                <h1>Create New Account</h1>
-                <div className={classes.flexInputs}>
-                    <div className={classes.inputwrapper}>
-                        <label htmlFor="FirstName">First Name</label>
-                        <input type="text" placeholder="Enter First Name..." id="FirstName" value={firstName} onChange={(e)=>{setFirstName(e.target.value)}}/>
+            <div style={{width: "-webkit-fill-available", margin: "80px 40px"}}>
+                <form onSubmit={(e)=> submit(e)}>
+                    <h1>Create New Account</h1>
+                    <div className={classes.flexInputs}>
+                        <div className={classes.inputwrapper}>
+                            <label htmlFor="FirstName">First Name</label>
+                            <input type="text" placeholder="Enter First Name..." id="FirstName" value={firstName} onChange={(e)=>{setFirstName(e.target.value)}}/>
+                        </div>
+                        <div className={classes.inputwrapper}>
+                            <label htmlFor="LastName">Last Name</label>
+                            <input type="text" placeholder="Enter Last Name..." id="LastName" value={lastName} onChange={(e)=>{setLastName(e.target.value)}}/>
+                        </div>
                     </div>
                     <div className={classes.inputwrapper}>
-                        <label htmlFor="LastName">Last Name</label>
-                        <input type="text" placeholder="Enter Last Name..." id="LastName" value={lastName} onChange={(e)=>{setLastName(e.target.value)}}/>
+                        <label htmlFor="Login">Login</label>
+                        <input  type="text" placeholder="Enter login..." id="Login" value={login} onChange={(e)=>{setLogin(e.target.value)}}/>
                     </div>
-                </div>
-                <div className={classes.inputwrapper}>
-                    <label htmlFor="Login">Login</label>
-                    <input type="text" placeholder="Enter login..." id="Login" value={login} onChange={(e)=>{setLogin(e.target.value)}}/>
-                </div>
-                <div className={classes.inputwrapper}>
-                    <label htmlFor="Password">Password</label>
-                    <input type="password" placeholder="Enter password..." id="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
-                </div>
-                <button className={classes.smallBtn}>
-                    <Link className={classes.textDecNone} to={"/"}>I already have an account</Link>
-                </button>
-                <button type="submit" disabled={login === "" || password === "" || firstName === "" || lastName === "" || password.length < 8 && true} className={login === "" || password === "" || firstName === "" || lastName === "" || password.length < 8 ? classes.loginBtnDisabled : classes.loginBtnActive}>
-                    <h5>Create Account</h5>
-                </button>
-            </form>
+                    <div className={classes.inputwrapper}>
+                        <label htmlFor="Password">Password</label>
+                        <input type="password" placeholder="Enter password..." id="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
+                    </div>
+                    <button className={classes.smallBtn}>
+                        <Link className={classes.textDecNone} to={"/"}>I already have an account</Link>
+                    </button>
+                    <button type="submit" disabled={login === "" || password === "" || firstName === "" || lastName === "" || password.length < 8 && true} className={login === "" || password === "" || firstName === "" || lastName === "" || password.length < 8 ? classes.loginBtnDisabled : classes.loginBtnActive}>
+                        <h5>Create Account</h5>
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
