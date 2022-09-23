16a3dad33d147a8cc755361c04dd73dbe8ddea29
KeeYoo <poronko2045@gmail.com>
2022-09-23 12:41:28 +0300

    React starting project is created

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -3,7 +3,7 @@ using Backend.Core.Services;
 using Backend.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
-; var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
