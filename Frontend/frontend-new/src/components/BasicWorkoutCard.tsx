ea3f10c47df5d15fef61adf881e594e59b1822f4
KeeYoo <poronko2045@gmail.com>
2022-09-25 19:13:18 +0300

    Fires count is fixed

--- Frontend/frontend-new/src/components/BasicWorkoutCard.tsx
+++ Frontend/frontend-new/src/components/BasicWorkoutCard.tsx
@@ -4,12 +4,13 @@ import classes from './BasicWorkoutCard.module.css'
 import {AiFillFire, AiOutlineFire} from 'react-icons/ai'
 
 const BasicWorkoutCard = (workout: IBasicalWorkout) => {
-
-    console.log(workout.topEfficiency)
     const [firstKey, setFirstKey] = useState("")
     const [secondKey, setSecondKey] = useState("")
     const [firstEfficiency, setFirstEfficiency] = useState(0)
     const [secondEfficiency, setSecondEfficiency] = useState(0)
+
+    const [firstArr, setFirstArr] = useState<number[]>([])
+    const [secondArr, setSecondArr] = useState<number[]>([])
 
     useEffect(() => {
         const getEfficiencies = () => {
@@ -19,22 +20,37 @@ const BasicWorkoutCard = (workout: IBasicalWorkout) => {
             let values = Object.values(workout.topEfficiency)
             setFirstEfficiency(values[0])
             setSecondEfficiency(values[1])
+            setFirstArr(Array(a(values[0])).fill(0));
+            setSecondArr(Array(a(values[1])).fill(0));
         }
         getEfficiencies()
     }, [])
 
+    const a = (number: number) => {
+        return 5 - number;
+    }
 
     return (
         <div className={classes.wrapper}>
             <img className={classes.img} src={workout.image} alt=""/>
             <div className={classes.workoutName}>{workout.name}</div>
             <div className={classes.efficiencyWrapper}>
-                <p className={classes.p}>{firstKey === "" ? "Empty" : firstKey}{firstEfficiency === 0 ? "Empty" : <span style={{paddingLeft: '10px'}}>{Array(firstEfficiency).fill(0).map((x,i) =>
-                    <AiFillFire/>
-                )}</span>}</p>
-                <p className={classes.p}>{secondKey === "" ? "Empty" : secondKey}{secondEfficiency === 0 ? "Empty" : <span style={{paddingLeft: '10px'}}>{[Array(secondEfficiency).fill(0).map((x,i) =>
-                    <AiFillFire/>
-                )]}</span>}</p>
+                <p className={classes.p}>{firstKey === "" ? "Empty" : firstKey}{firstEfficiency === 0 ? "Empty" :
+                    <span style={{paddingLeft: '10px'}}>{Array(firstEfficiency).fill(0).map((x,i) =>
+                        <AiFillFire key={i}/>
+                    )}</span>}
+                    <span>{firstArr.map((x,i) =>
+                        <AiOutlineFire key={i}/>
+                    )}</span>
+                </p>
+                <p className={classes.p}>{secondKey === "" ? "Empty" : secondKey}{secondEfficiency === 0 ? "Empty" :
+                    <span style={{paddingLeft: '10px'}}>{Array(secondEfficiency).fill(0).map((x,i) =>
+                        <AiFillFire key={i}/>
+                    )}</span>}
+                    <span>{secondArr.map((x,i) =>
+                        <AiOutlineFire key={i}/>
+                    )}</span>
+                </p>
             </div>
         </div>
     );
